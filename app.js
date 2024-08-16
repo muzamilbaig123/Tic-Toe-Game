745682621b470c4c59a048908163678bedca48ab
= <amberoortiz@gmail.com>
2024-08-16 09:41:00 -0700

    all file added

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let check = document.querySelectorAll(".check")[0];
+function myFun() {
+    check.innerHTML = `99`;
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+let check = document.querySelectorAll(".check")[0];
+
+function myFun () {
+    check.innerHTML = `99`
+}
